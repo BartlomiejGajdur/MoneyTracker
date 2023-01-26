1023b98f022e12f67ffa425d347e757501259bf8
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2023-01-26 19:10:01 +0100

    Validation of Date added

--- Src/Date.cpp
+++ Src/Date.cpp
@@ -23,6 +23,16 @@ DateErrorCode Date::setDate(int day, int month, int year){
     if(month < 0 || month > 12)
         return DateErrorCode::InvalidMonth;
     month_ = month;
-    
-    
+
+    if( 
+        ((month_ == 1 || month_ == 3 || month_ == 5 || month_ == 7 || month_ == 8 || month_ == 10 || month_ == 12) && day <=31 && day >=0) ||
+        ((month_ == 4 || month_ == 6 || month_ == 9 || month_ == 11) && day <=30 && day >=0) ||
+        (month_ == 2 && this->isLeapYear() && day <=29 && day >=0) ||
+        (month_ == 2 && !this->isLeapYear() && day <=28 && day >=0))
+        {
+            day_ = day;
+            return DateErrorCode::Ok;
+        }
+        return DateErrorCode::InvalidDay;
+            
 }
