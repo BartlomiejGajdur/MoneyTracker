010652550c36cd27cfb18fdd8236baad4f09542b
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2023-01-26 18:46:59 +0100

    current Data added

--- Include/Date.hpp
+++ Include/Date.hpp
@@ -1,12 +1,28 @@
-enum class ErrorCode { Ok };
+
+#include <string>
+#include <ctime>
+enum class DateErrorCode { Ok, InvalidCode };
 
 class Date{
 public:
     Date() = default;
     Date(int day, int month, int year): day_(day) , month_(month), year_(year) {};
 
+    //Setters   
+    void setDay(int day)        { day_ = day;}
+    void setMonth(int month)    { month_ = month;}
+    void setYear(int year)      { year_ = year;}
+
+    //Getters
+    int getDay()   const   { return day_;}
+    int getMonth() const   { return month_;}
+    int getYear()  const   { return year_ ;}
+
+    //Functions
+    static std::string currentData();
+
 private:
+    int day_{};
     int month_{};
     int year_{};
-    int day_{};
 };
--- Src/Date.cpp
+++ Src/Date.cpp
@@ -1,2 +1,11 @@
 #include "../Include/Date.hpp"
 
+std::string Date::currentData(){
+
+    time_t t = time(nullptr);
+    tm* timePtr = localtime(&t);
+ 
+    return std::to_string(timePtr->tm_mday) + "-" +
+           std::to_string(1+timePtr->tm_mon) + "-" + 
+           std::to_string(1900+ timePtr->tm_year); 
+}
