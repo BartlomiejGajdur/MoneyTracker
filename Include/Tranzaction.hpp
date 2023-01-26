79301530725a19dff484b7b1c38e2b84b78aeff3
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2023-01-26 18:03:16 +0100

    Setters added

--- Include/Tranzaction.hpp
+++ Include/Tranzaction.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include <string>
 
-enum class ExpenseCategory { Housing = 1, Transportation, Food, Utilities, Insurance, Medical, Savings, PersonalSpending, Entertainment, Miscellaneous};
+extern enum class ExpenseCategory { Housing = 1, Transportation, Food, Utilities, Insurance, Medical, Savings, PersonalSpending, Entertainment, Miscellaneous};
+extern enum class ErrorCode { Ok };
+
 
 class Tranzaction{
 public:
@@ -12,15 +14,17 @@ public:
                                                           money_(money),
                                                           expenseCategory_(expenseCategory) {ID_++;};
 
-    static size_t ID_;
-    
-private:
-    
-    ExpenseCategory expenseCategory_;
-    std::string description_;
-    double money_;
+    //Setters
+        void setExpenseCategory(const ExpenseCategory& expenseCategory) { expenseCategory_ = expenseCategory; }
+        void setDescription(const std::string& description)             { description_ = description;}
+        void setMoney(const double& money_)                             { money_ = money;}
 
     
-    //static size_t counter;
+private:
+
+    static size_t ID_;
+    ExpenseCategory expenseCategory_{ExpenseCategory::Miscellaneous};
+    std::string description_;
+    double money_;
 
 };
