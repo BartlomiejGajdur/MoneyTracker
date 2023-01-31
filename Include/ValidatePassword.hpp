2839cd7e6efbf13ffaddeafbd3536d08daa4e767
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2023-01-31 14:02:01 +0100

    Update

--- Include/ValidatePassword.hpp
+++ Include/ValidatePassword.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <conio.h>
 #include "bcrypt.h"
 
 using vecConfig = std::vector<std::tuple<std::string,std::string,std::string>>;
@@ -13,6 +14,7 @@ namespace ValidatePassword{
 
     vecConfig vecOfFileNameLoginPassword_;
     std::string configName = "../config.txt";
+    std::tuple<std::string,std::string,std::string> currentUser;
    
     void loadConfigFromFile(){
 
@@ -79,13 +81,54 @@ namespace ValidatePassword{
             std::string PersonalConfigName = "config" + std::to_string(vecOfFileNameLoginPassword_.size()+1);
 
             vecOfFileNameLoginPassword_.push_back(std::make_tuple(PersonalConfigName,bcrypt::generateHash(Login), bcrypt::generateHash(Password)));
-            
+            saveConfigToFile();
             return true;
         }else{
-            std::cout<<"PODANY LOGIN JUZ ISTNIEJE W BAZIE";
             return false;
         }
             
+    }
+
+    bool logIn(const std::string& Login, const std::string& Password){
+        auto it = std::find_if(vecOfFileNameLoginPassword_.begin(),vecOfFileNameLoginPassword_.end(),[&](auto user)
+                                                                                                        {
+                                                                                                            return bcrypt::validatePassword(Login,std::get<1>(user)) && bcrypt::validatePassword({Password},std::get<2>(user)) ;
+                                                                                                        });
+
+        if(it != vecOfFileNameLoginPassword_.end()){
+            currentUser = std::make_tuple(std::get<0>(*it), Login, std::get<2>(*it));
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //To będzie raczej w jakims MENU CZY COS
+    std::string insertPassword(){
+        std::string password{};
+        char ch;
+
+        do{
+            ch = _getch();
+            if(ch == 8)
+            {
+                if(password.size()>0)
+                {
+                    std::cout<<"\b \b";
+                    password.pop_back();
+                }
+                
+            }else if(ch == 13){
+
+            }
+            else{
+                std::cout<<"*";
+                password+=ch;
+            }
+
+        }while(ch != 13);
+
+        return password;
     }
 
    
