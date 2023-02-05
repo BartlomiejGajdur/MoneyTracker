1f22699806b87be4e89a90f87b8ee86d443cc0f9
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2023-02-05 21:21:44 +0100

    Bills Class added

---
+++ Include/Bills.hpp
@@ -0,0 +1,12 @@
+#include "Obligations.hpp"
+
+enum class BillType {Electricity = 0, Water, Gas, Internet };
+
+class Bills : public Obligations{
+
+public:
+    
+
+private:
+    BillType billType_;
+};
