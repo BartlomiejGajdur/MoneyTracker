d91b717765dc5996c09fba1672e3fb1c6dd03c24
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2023-02-23 17:20:41 +0100

    Loan print

--- Src/Loan.cpp
+++ Src/Loan.cpp
@@ -1,4 +1,6 @@
 #include "../include/Loan.hpp"
+#include "../include/MenuFunctions.hpp"
+#include <iomanip>
 #include <sstream>
 
 void Loan::printObligation(std::ostream& os) const{
@@ -6,7 +8,27 @@ void Loan::printObligation(std::ostream& os) const{
 }
 
 std::string Loan::printObligation() const{
-    return description_ + " - " + std::to_string(moneyToPay_)+ " - "+ paymentDate_.dateInString() + " Pozostalo RAT "+ std::to_string(numberOfInstallments_) + "\n";
+
+    std::stringstream is;
+    std::string day;
+
+    is<<"| "<<std::setw(12)<< std::left<< description_<<" | "
+    <<std::setw(12) << std::left<<std::to_string(moneyToPay_)<<" | "
+    <<std::setw(12) << std::left<<paymentDate_.dateInString() <<" | ";
+    this->distanceToPayDate() == 1 || this->distanceToPayDate() == -1 ? day = " day" : day = " days";
+
+    if(this->distanceToPayDate() > 0 && this->distanceToPayDate() <=7)
+    {
+        is<<std::setw(20) << std::left<<MenuFunctions::SetTextColor(Color::Yellow, std::to_string(this->distanceToPayDate()) + day)<<" | ";
+    }else if(this->distanceToPayDate() > 7){
+        is<<std::setw(20) << std::left<<MenuFunctions::SetTextColor(Color::Green, std::to_string(this->distanceToPayDate()) + day)<<" | ";
+    }else{
+        is<<std::setw(20) << std::left<<MenuFunctions::SetTextColor(Color::Red, std::to_string(this->distanceToPayDate()) + day)<<" | ";
+    }
+
+    is<<std::setw(16) << std::left<<std::to_string(numberOfInstallments_) +  " left" <<" |\n";
+
+    return is.str();
 }
 
 std::ostream& operator<<(std::ostream &os, const Loan& Loan){
